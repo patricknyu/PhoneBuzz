8765900b39887c394e9152577ba2797f5c96ee60
patricknyu <patrickyuemail@gmail.com>
2015-12-16 12:43:10 -0800

    still playing with it

--- run.py
+++ run.py
@@ -1,13 +1,11 @@
-import os
 from flask import Flask
-import twilio.twiml
-
+import twilio.twiml as twmil
 app = Flask(__name__)
 
 @app.route("/",methods = ['GET','POST'])
 def hello_monkey():
 	"""Respond to incoming requests."""
-	resp = twilio.twiml.Response()
+	resp = twiml.Response()
 	resp.say("Hello Monkey")
 
 	return str(resp)

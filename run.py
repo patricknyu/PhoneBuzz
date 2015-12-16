46102c02f2e33ffd94e2033bb09ee75ab96e3045
patricknyu <patrickyuemail@gmail.com>
2015-12-16 14:03:35 -0800

    fixing more

--- run.py
+++ run.py
@@ -1,14 +1,14 @@
 from flask import Flask
-import twilio.twiml as twmil
+import twilio.twiml
 app = Flask(__name__)
 
 @app.route("/",methods = ['GET','POST'])
 def hello_monkey():
 	"""Respond to incoming requests."""
-	resp = twiml.Response()
+	resp = twilio.twiml.Response()
 	resp.say("Hello Monkey")
 
 	return str(resp)
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(debug=False)

2f3aef808a9019c183a6ecbd8fc5b95ef72a61b0
patricknyu <patrickyuemail@gmail.com>
2015-12-17 11:40:29 -0800

    adding a press key to connect to real monkey

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, redirect
+#from __future__ import with_statement
 import twilio.twiml
+
 app = Flask(__name__)
 
 callers = {
@@ -22,8 +24,21 @@ def hello_monkey():
 	resp.say("Hello " + caller)
 	#Play an MP3
 	resp.play("http://demo.twilio.com/hellomonkey/monkey.mp3")
-
+	
+	with resp.gather(numDigits=1, action = "/handle-key",method= "POST") as g:
+		g.say("To speak to a real monkey, press 1.  Press any other key to start over.")
 	return str(resp)
+@app.route("/handle-key",methods = ['GET','Post'])
+def handle_key():
+	digit_pressed = request.values.get('Digits',None)
+	if digit_pressed == "1":
+		resp = twilio.twiml.Response()
+		resp.dial("+13105551212")
+		resp.say("The call failed, or the remote party hung up.  Goodbye.")
+
+		return str(resp)
+	else:
+		return redirect("/")
 
 if __name__ == "__main__":
 	app.run(debug=False)

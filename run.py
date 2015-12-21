26c528f01ebf397967382671cba4544a238bdaee
patricknyu <patrickyuemail@gmail.com>
2015-12-21 01:37:32 -0800

    putting in replay

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from twilio.rest import TwilioRestClient
 import twilio.twiml
 import validator
 import twilio_client
-
+from time import gmtime, strftime
 app = Flask(__name__)
 
 callers = {
@@ -26,27 +26,6 @@ def html_render():
     	global callRequests
     	return render_template('index.html',history =history,callRequests=callRequests)
 
-"""@app.route("/hello_monkey",methods = ['GET','POST'])
-def hello_monkey():
-
-	#Get the callers number
-	from_number = request.values.get('From',None)
-	resp = twilio.twiml.Response()
-	
-	#check if the caller is someone we know
-	if from_number in callers:
-		caller = callers[from_number]
-	else:
-		caller = "Monkey"
-	
-	resp.say("Hello " + caller)
-	#Play an MP3
-	resp.play("http://demo.twilio.com/hellomonkey/monkey.mp3")
-	
-	with resp.gather(finishOnKey = '#', action = "/handle-key",method= "POST") as g:
-		g.say("Press a number. Then press pound.  I will return Fizz Buzz up to that number.")
-	return str(resp)
-"""
 @app.route("/call",methods = ["POST"])
 def call():
 	currentTime = request.args.get('time')
@@ -63,14 +42,6 @@ def handle_key():
 
 	callRequests[currentTime] += (digit_pressed,)
 
-	"""if digit_pressed == "1":
-		resp = twilio.twiml.Response()
-		resp.dial("+13105551212")
-		resp.say("The call failed, or the remote party hung up.  Goodbye.")
-
-		return str(resp)
-	else:
-		return redirect("/")"""
 	def int_to_fizzbuzz(i):
 		ans = ''
 		if(i%3==0):
@@ -85,6 +56,28 @@ def handle_key():
 		resp.say(str(int_to_fizzbuzz(x)))
 	return str(resp)
 
+@app.route("/replay",method=['POST'])
+def replay():
+	global history
+	global callRequests
+
+	num = request.form['Digits']
+	phoneNum = request.form['Phone']
+
+	currentTime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+
+	history.append(currentTime)
+	#delay is 0
+	callRequests[currentTime] = (0,phoneNum)
+	account_sid = "AC293ba385dfd140435b955c184eb6b7a7"
+	auth_token = "ae6f6c20e96fc7a9803a100292ab5284"
+	client = TwilioRestClient(account_sid, auth_token)
+	
+	#create the call similarly to before, just force the digits given.
+	client.calls.craete(to=phoneNum, # The given phone number
+	from_ = "5167145942", #Tilio number
+	url=request.url_root+"call?time="+currentTime,send_digits = num)
+	return ""
 @app.route("/make_call",methods=["POST"])
 def make_call():
 	global history
